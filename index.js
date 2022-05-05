3488747f18466aca75edc2ee1326419fa8e7ebc6
unknown <md616264@gmail.com>
2022-05-05 18:21:25 +0600

    jwt complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -8,8 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//assignment-11
-//qtfVlsIorLmAmtpD
+
+function verifyJWT(req, res, next) {
+    const authHeaders = req.headers.authorization;
+    if (!authHeaders) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeaders.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    });
+};
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8xa8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -20,12 +35,23 @@ async function run() {
         await client.connect();
         const servicesCollection = client.db("assignment-11").collection("services");
 
-        app.get('/services', async (req, res) => {
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
+        //SERVICES API
+        app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find()
             const services = await cursor.toArray();
             res.send(services)
         });
+
+        //SINGLE SERVICE API
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -33,7 +59,7 @@ async function run() {
             res.send(result);
         });
 
-        //update user
+        //QUANTITY UPDATE API 
         app.put('/service/:id', async (req, res) => {
             const id = req.params.id;
             const user = req.body;
@@ -47,6 +73,8 @@ async function run() {
             result = await servicesCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         });
+
+        //QUANTITY DELIVERY API
         app.put('/delivery/:id', async (req, res) => {
             const id = req.params.id;
             const user = req.body;
@@ -61,25 +89,33 @@ async function run() {
             result = await servicesCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         });
+
+        //ITEM DELETE API
         app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await servicesCollection.deleteOne(query);
             res.send(result);
         });
-        //Add Item
+        //ADD ITEM API
         app.post('/add', async (req, res) => {
             const newItem = req.body;
             const result = await servicesCollection.insertOne(newItem);
             res.send(result);
         });
-        app.get('/add', async (req, res) => {
+        //JWT API
+        app.get('/add', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            // console.log('email', email);
-            const query = { email: email };
-            const cursor = servicesCollection.find(query);
-            const addItems = await cursor.toArray();
-            res.send(addItems)
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = servicesCollection.find(query);
+                const addItems = await cursor.toArray();
+                res.send(addItems)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
         })
 
     }
